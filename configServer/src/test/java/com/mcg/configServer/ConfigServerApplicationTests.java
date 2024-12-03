3d4ad4a3ee3ecf8a81f802f9fd2fb1902ac841d9
IngMariCruz <ingmarianacruz@gmail.com>
2024-12-03 02:22:56 -0500

    finish configServer

---
+++ configServer/src/test/java/com/mcg/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcg.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
